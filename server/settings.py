d1e8ff199356cfcc78a955a568bcad426119d78c
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-02-07 17:02:41 +0600

    added redoc

--- server/settings.py
+++ server/settings.py
@@ -167,4 +167,6 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://restaurant-management-app.vercel.app",
+    "https://admin-restaurant-management.vercel.app",
 ]
