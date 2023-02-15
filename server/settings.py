5675ec60995e73aaab4817b119538570d8965bc6
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-02-15 21:16:06 +0600

    added allowed hosts

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "restaurant-management.studyservice.co.uk"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "restaurant-management.studyservice.co.uk",
+    "restaurant-management-api-production.up.railway.app",
+]
 
 
 # Application definition
