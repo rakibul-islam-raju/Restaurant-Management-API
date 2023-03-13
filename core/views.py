67b5757519dbc2154292ac9af755605c8573581a
Rakibul Islam <rakibul.islam7772588@gmail.com>
2023-03-13 23:27:57 +0600

    [FIX] fixed reservation permission issue

--- core/views.py
+++ core/views.py
@@ -301,7 +301,7 @@ class ResarvationListCreateView(ListCreateAPIView):
         if self.request.method == "POST":
             self.permission_classes = [IsAuthenticated]
         else:
-            self.permission_classes = [IsAdminUser, IsOwner]
+            self.permission_classes = [IsStaffOrOwnerAuthenticated]
 
         return super(ResarvationListCreateView, self).get_permissions()
 
